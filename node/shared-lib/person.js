384d3f1bda3aeb17101f4dee79e18740d11eb598
Ryan Silowash <resilowash@gmail.com>
2017-10-14 22:58:39 -0400

    added shared library and first quick test

---
+++ node/shared-lib/person.js
@@ -0,0 +1,8 @@
+var Person = function(firstName, lastName, birthYear, email) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.birthyear = birthYear;
+  this.email = email;
+}
+
+module.exports = Person; 
---
+++ node/testing1/app.js
@@ -0,0 +1,6 @@
+//first node test
+const Person = require('../shared-lib/person.js');
+
+var person = new Person("Cosmo", "Kramer", 1960, "cosmo@vandalay.com");
+
+console.log(person);
