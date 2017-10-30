2e435f5107ebdfa2418f46c45f43990b7205e368
Ryan Silowash <resilowash@gmail.com>
2017-10-29 21:31:27 -0400

    working on 'inheritance' model in javascript

--- node/testing1/app.js
+++ node/testing1/app.js
@@ -6,3 +6,10 @@ var person = new Person("Cosmo", "Kramer", 1960, "cosmo@vandalay.com");
 console.log(person);
 console.log(person.introduceSelf());
 console.log(person.question("Bill"));
+
+//extending person outside of it's base. 
+Person.prototype.Hey = function() {
+  return "Hey, " + this.firstName + " WAKE UP!";
+}
+
+console.log(person.Hey());
